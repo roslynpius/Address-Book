8f67519bcc67393684502e4e6d3060ee581ad647
roslynpius <roslyn.pius@gmail.com>
2023-11-24 21:20:52 +0530

    Implemented USECASE 1: Creation of Contact.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,9 +1,101 @@
+import java.util.Scanner;
+
+/**
+ * The Contact class represents a person's contact information.
+ * It includes fields for the first name, last name, address,
+ * city, state, ZIP code, phone number, and email. *
+ */
+class Contact {
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    String zip;
+    String phoneNumber;
+    String email;
+
+    /**
+     * Constructs a new Contact with the specified information.
+     *
+     * @param firstName   The first name of the contact.
+     * @param lastName    The last name of the contact.
+     * @param address     The address of the contact.
+     * @param city        The city of the contact.
+     * @param state       The state of the contact.
+     * @param zip         The ZIP code of the contact.
+     * @param phoneNumber The phone number of the contact.
+     * @param email       The email address of the contact.
+     */
+    public Contact(String firstName, String lastName, String address, String city, String state, String zip,
+                   String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    /**
+     * Displays the contact information on the console.
+     */
+    public void displayContact() {
+        System.out.println();
+        System.out.println("Contact Information:");
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Address: " + address);
+        System.out.println("City: " + city);
+        System.out.println("State: " + state);
+        System.out.println("ZIP Code: " + zip);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Email: " + email);
+    }
+}
+
 /**
  * The AddressBook class represents a digital collection of contact information for individuals.
  * It provides functionality to add, remove, and edit contacts.
  */
 public class AddressBook {
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program!!");
+        Scanner scanner = new Scanner(System.in);
+
+        // Get contact information from user
+        System.out.println("Enter Contact Information:");
+        System.out.print("First Name: ");
+        String firstName = scanner.nextLine();
+
+        System.out.print("Last Name: ");
+        String lastName = scanner.nextLine();
+
+        System.out.print("Address: ");
+        String address = scanner.nextLine();
+
+        System.out.print("City: ");
+        String city = scanner.nextLine();
+
+        System.out.print("State: ");
+        String state = scanner.nextLine();
+
+        System.out.print("ZIP Code: ");
+        String zip = scanner.nextLine();
+
+        System.out.print("Phone Number: ");
+        String phoneNumber = scanner.nextLine();
+
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+
+        // Create a new contact
+        Contact newContact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+
+        // Display the contact information
+        newContact.displayContact();
+
+        scanner.close();
     }
 }
