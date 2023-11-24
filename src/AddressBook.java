21b462d59bf13878489e0a8d96e5dc80dfd9b6ed
roslynpius <roslyn.pius@gmail.com>
2023-11-24 21:00:46 +0530

    Initial Commit

---
+++ src/AddressBook.java
@@ -0,0 +1,9 @@
+/**
+ * The AddressBook class represents a digital collection of contact information for individuals.
+ * It provides functionality to add, remove, and edit contacts.
+ */
+public class AddressBook {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program!!");
+    }
+}
